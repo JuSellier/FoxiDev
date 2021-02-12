fa6fd10eef53dc824fa441fabfc0f729968ea459
JuSellier <juju.sel.mail@gmail.com>
2021-02-12 14:21:42 +0100

    adjusted images and uris for project previews

--- src/components/ProjectPreview/ProjectPreview.js
+++ src/components/ProjectPreview/ProjectPreview.js
@@ -1,22 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./ProjectPreview.scss";
 import { Link } from "react-router-dom";
 import { ImEye as EyeIcon } from "react-icons/im";
 
-const ProjectPreview = ({ children, title, imgUrl, link = "/" }) => {
+const ProjectPreview = ({ children, title, imgUrl, uri = "/" }) => {
+  const history = useHistory();
+
   return (
     <section className="ProjectPreview">
       <div
         className="ProjectPreview-Image"
         style={{ backgroundImage: `url(${imgUrl})` }}
       >
-        <div className="ProjectPreview-Image-View">
+        <div
+          className="ProjectPreview-Image-View"
+          onClick={() => history.push(uri)}
+        >
           <EyeIcon />
         </div>
       </div>
       <div className="ProjectPreview-Title">{title}</div>
       <div className="ProjectPreview-Children">{children}</div>
-      <Link to={link} className="ProjectPreview-Link button-hover-gradient">
+      <Link to={uri} className="ProjectPreview-Link button-hover-gradient">
         View project
       </Link>
     </section>
--- src/pages/Home/HomePortfolio/HomePortfolio.js
+++ src/pages/Home/HomePortfolio/HomePortfolio.js
@@ -9,41 +9,52 @@ const HomePortfolio = () => {
       <p className="fade-in">Explore some of my projects below.</p>
       <SwipeMenu>
         <ProjectPreview
-          title="Zenbot"
+          title="ZenSky"
           imgUrl="https://images.unsplash.com/photo-1577253313708-cab167d2c474?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1022&q=80"
         >
-          <p>The mental health app.</p>
+          <p>
+            A simple app to track and visualise your dreams and your emotions
+            through time.
+          </p>
         </ProjectPreview>
 
         <ProjectPreview
-          title="Algorithmic Art"
+          title="NeoCraft"
+          uri="neocraft"
           imgUrl="https://images.unsplash.com/photo-1510906594845-bc082582c8cc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1028&q=80"
         >
-          <p>An algorithmic art project.</p>
+          <p>
+            An collaborative algorithmic art project, aimed at promoting
+            creative usage of computer algorithms and tech in arts.
+          </p>
         </ProjectPreview>
 
         <ProjectPreview
-          title="Voice to Text for Chrome"
+          title="NeoPass"
+          imgUrl="https://images.unsplash.com/photo-1582139329536-e7284fece509?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+        >
+          <p>A minimal password generator as a chrome extension.</p>
+        </ProjectPreview>
+
+        <ProjectPreview
+          title="V2T.app"
           imgUrl="https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
         >
-          <p>
-            A minimal web app to transcribe your voice to text and copy it to
-            your clipboard.
-          </p>
+          <p>A simple voice to text utility as a website.</p>
         </ProjectPreview>
 
         <ProjectPreview
           title="Surf Camp"
           imgUrl="https://images.pexels.com/photos/390051/surfer-wave-sunset-the-indian-ocean-390051.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         >
-          <p>A prototype for a surf camp wesite.</p>
+          <p>A showcase website for a surf camp business.</p>
         </ProjectPreview>
 
         <ProjectPreview
-          title="Guitar Lessons"
-          imgUrl="https://images.pexels.com/photos/3987249/pexels-photo-3987249.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          title="Coworking spaces"
+          imgUrl="https://images.unsplash.com/photo-1612831454918-f65c3ecfc34d?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
         >
-          <p>A prototype for a guitar courses business website.</p>
+          <p>A showcase website for coworking spaces.</p>
         </ProjectPreview>
       </SwipeMenu>
     </section>
